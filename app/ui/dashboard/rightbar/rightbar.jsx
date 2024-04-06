73a635bd7ab11f3cca91a3e14e341e5c57dd50c7
Parth Umrania <pumrania9@gmail.com>
2024-04-06 09:30:13 +0530

    Working on right bar

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,7 +1,49 @@
+import { MdPlayCircleFilled, MdReadMore } from 'react-icons/md';
 import styles from './rightbar.module.scss';
+import Image from "next/image";
 
 export default function Rightbar() {
   return (
-    <div>rightbar</div>
+    <div className={styles.rightbar}>
+      <div className={styles.item}>
+        <div className={styles.bgRightbar}>
+          <Image src="/astronaut.png" alt='' fill className={styles.bgImg} />
+        </div>
+        <div className={styles.info}>
+          <span className={styles.notification}>🔥 Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of the admin dashboard?
+          </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit eius libero perspiciatis recusandae possimus.
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled />
+            Watch
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.info}>
+          <span className={styles.notification}>🚀 Coming Soon</span>
+          <h3 className={styles.title}>
+            New server actions are available, partial pre-rendering is coming
+            up!
+          </h3>
+          <span className={styles.subtitle}>Boost your productivity</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit eius libero perspiciatis recusandae possimus.
+          </p>
+          <button className={styles.button}>
+            <MdReadMore />
+            Learn
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
